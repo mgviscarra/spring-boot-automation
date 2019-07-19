86d5ae502290750c99036c259c6abda2dae514ad
Mauricio Viscarra <mauricio.viscarra@truextend.com>
2019-07-19 16:42:46 -0400

    Adding support for docker

--- src/test/java/stepDefinitions/Browser.java
+++ src/test/java/stepDefinitions/Browser.java
@@ -3,14 +3,19 @@ package stepDefinitions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
 
 public class Browser {
 
     public static WebDriver launch() {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--incognito");
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\mauricio.viscarra\\Documents\\ChromeDriver\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver(options);
+        //options.addArguments("--incognito");
+        String host = "http://localhost:4444/wd/hub";
+        options.setAcceptInsecureCerts(true);
+        //System.setProperty("webdriver.chrome.driver", "C:\\Users\\mauricio.viscarra\\Documents\\ChromeDriver\\chromedriver.exe");
+        RemoteWebDriver driver = new RemoteWebDriver(new URL(host),options);
         return driver;
     }
 }
